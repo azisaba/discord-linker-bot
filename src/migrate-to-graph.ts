7b1fdedd2f46eb0c2ffd8a5d6691ae14cd81f60f
tksimeji <tksimeji@outlook.com>
2026-07-17 17:03:51 +0900

    commit for now

--- src/migrate-to-graph.ts
+++ src/migrate-to-graph.ts
@@ -8,7 +8,7 @@ type PlayerRecord = {
 
 export async function migrateToGraph(connection: Connection) {
   console.log('Migrating Discord links to graph');
-  console.log('foobar');
+  console.log('foobar!');
 
   const apiKey = process.env.GRAPH_API_KEY;
   if (!apiKey) {
